0b28976fcda90a74edf8161862b7e3c35515cdfb
Francisco J. Contreras <cs21016@ues.edu.sv>
2022-03-18 10:02:51 -0600

    Cambios menores

--- src/guia/Guia2.java
+++ src/guia/Guia2.java
@@ -4,7 +4,9 @@ package guia;
 public class Guia2 {
     
     public static void main(String[] args) {
+        //Hacer una clase Password con atributos de longitud y contraseña, con constructores y métodos
         
-    }
-    
+            
+        }
 }
+    
