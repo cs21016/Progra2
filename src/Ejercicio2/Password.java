e751271c6b22bcb78062af29ff7d1aca0a8f502a
Francisco J. Contreras <cs21016@ues.edu.sv>
2022-03-18 11:31:52 -0600

    Generando la clase Password con sus atributos y métodos

--- src/Ejercicio2/Password.java
+++ src/Ejercicio2/Password.java
@@ -1,7 +1,36 @@
 
 package Ejercicio2;
 //Francisco José Contreras Sanabria - CS2016 - PGRII
-public class Password {
+public class Password { //Crear una clase Password que contenga atributos de longitud, contraseña, constructores y métodos
+    //Atributos
+    private int longitud;
+    String contrasena;
+    private final int LONG_DEF=8;
     
-    
+    //Insertamos los Constructores
+    public Password() {
+        this.longitud = LONG_DEF;
+        this.contrasena = contrasena;
+    }
+    public Password(int longitud) {
+        this.longitud = longitud;
+        this.contrasena = generarPassword();
+    }
+    //Insertamos los Métodos
+    public String getcontrasena(){
+        return contrasena;
+    }
+    public int getLongitud() {
+        return longitud;
+    }
+    public void setLongitud(int longitud){
+        this.longitud = longitud;
+    }
+    public void setContrasena(String contrasena){
+        this.contrasena = contrasena;
+    }
+    private String generarPassword(){
+        contrasena = "";
+        return contrasena;
+    }
 }
