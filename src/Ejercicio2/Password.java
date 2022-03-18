3bdbd74446b434cdb7f636c4f4523ff8c5d9eb76
Francisco J. Contreras <cs21016@ues.edu.sv>
2022-03-18 15:57:52 -0600

    Añadiendo valor booleano y agregando clase ejecutable

--- src/Ejercicio2/Password.java
+++ src/Ejercicio2/Password.java
@@ -5,7 +5,10 @@ public class Password { //Crear una clase Password que contenga atributos de lon
     //Atributos
     private int longitud;
     String contrasena;
-    private final int LONG_DEF=8;
+    private final int LONG_DEF = 8;
+    private final static int SECURE_PASSWD_NUM = 5;
+    private final static int SECURE_PASSWD_MAYUS = 2;
+    private final static int SECURE_PASSWD_MINUS = 1;
     
     //Insertamos los Constructores
     public Password() {
@@ -31,6 +34,37 @@ public class Password { //Crear una clase Password que contenga atributos de lon
     }
     private String generarPassword(){
         contrasena = "";
+        for (int i=0;i<longitud;i++){
+            switch ((int)(Math.random()*3)){
+                case 0:
+                    char minusculas=(char)((int)Math.floor(Math.random()*(123-97)+97));
+                    contrasena+=minusculas;
+                case 1:
+                   char mayusculas=(char)((int)Math.floor(Math.random()*(91-65)+65));
+                   contrasena+=mayusculas;
+                case 3:
+                   char numeros=(char)((int)Math.floor(Math.random()*(58-48)+48));
+                   contrasena+=numeros;
+            }
+        }
         return contrasena;
+    }
+    //Comprobamos si la contraseña es fuerte o no con un valor booleano
+    public boolean esFuerte(){
+        int contNum=0, contMinus=0, contMayus=0;
+        for (int i = 0; i < this.longitud; i++){
+            if (contrasena.charAt(i)>=97 && contrasena.charAt(i)<=122){
+                contMinus+=1;
+            }
+            else {
+                if (contrasena.charAt(i)>=65 && contrasena.charAt(i)<=90){
+                    contMayus+=1;
+            }
+            else {
+               contNum+=1; 
+            }
+        }
+    }
+        return contMayus > SECURE_PASSWD_MAYUS && contMinus > SECURE_PASSWD_MINUS && contNum > SECURE_PASSWD_NUM ? true : false;
     }
 }
