7605fa2a314fa3dcbffe4c40c3ac2e916e86c810
Francisco J. Contreras <cs21016@ues.edu.sv>
2022-03-18 18:19:03 -0600

    Corrigiendo errores y programando la clase ejecutable

--- src/Ejercicio2/Password.java
+++ src/Ejercicio2/Password.java
@@ -1,42 +1,58 @@
-
 package Ejercicio2;
 //Francisco José Contreras Sanabria - CS2016 - PGRII
+
 public class Password { //Crear una clase Password que contenga atributos de longitud, contraseña, constructores y métodos
     //Atributos
+
     private int longitud;
-    String contrasena;
-    private final int LONG_DEF = 8;
+    public String contrasena;
+    private final static int LONG_DEF = 8;
     private final static int SECURE_PASSWD_NUM = 5;
     private final static int SECURE_PASSWD_MAYUS = 2;
     private final static int SECURE_PASSWD_MINUS = 1;
-    
+
     //Insertamos los Constructores
     public Password() {
         this.longitud = LONG_DEF;
         this.contrasena = contrasena;
     }
+
     public Password(int longitud) {
         this.longitud = longitud;
         this.contrasena = generarPassword();
     }
+
     //Insertamos los Métodos
-    public String getcontrasena(){
+    public String getContrasena() {
         return contrasena;
     }
+
     public int getLongitud() {
         return longitud;
     }
-    public void setLongitud(int longitud){
+
+    public void setLongitud(int longitud) {
         this.longitud = longitud;
     }
-    public void setContrasena(String contrasena){
+
+    public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
     }
-    private String generarPassword(){
+
+    private String generarPassword() {
         contrasena = "";
-        for (int i=0;i<longitud;i++){
-            switch ((int)(Math.random()*3)){
+        for (int i = 0; i < longitud; i++) {
+            switch ((int) (Math.random() * 3)) {
                 case 0:
+                    contrasena += (char) (Math.random() * 26 + 'a');
+                    break;
+                case 1:
+                    contrasena += (char) (Math.random() * 26 + 'A');
+                    break;
+                case 2:
+                    contrasena += (char) (Math.random() * 10 + '0');
+                    break;
+                /* case 0:
                     char minusculas=(char)((int)Math.floor(Math.random()*(123-97)+97));
                     contrasena+=minusculas;
                 case 1:
@@ -44,16 +60,25 @@ public class Password { //Crear una clase Password que contenga atributos de lon
                    contrasena+=mayusculas;
                 case 2:
                    char numeros=(char)((int)Math.floor(Math.random()*(58-48)+48));
-                   contrasena+=numeros;
+                   contrasena+=numeros;*/
             }
         }
         return contrasena;
     }
     //Comprobamos si la contraseña es fuerte o no con un valor booleano
-    public boolean esFuerte(){
-        int contNum=0, contMinus=0, contMayus=0;
-        for (int i = 0; i < this.longitud; i++){
-            if (contrasena.charAt(i)>=97 && contrasena.charAt(i)<=122){
+
+    public boolean esFuerte() {
+        int contNum = 0, contMinus = 0, contMayus = 0;
+        for (int i = 0; i < this.longitud; i++) {
+            if(this.contrasena.charAt(i) >= 'A' && this.contrasena.charAt(i) <= 'Z')
+                contMayus++;
+            if (this.contrasena.charAt(i) >= 'a' && this.contrasena.charAt(i) <= 'z') {
+                contMinus++;
+            }
+            if (this.contrasena.charAt(i) >= '0' && this.contrasena.charAt(i) <= '9') {
+                contNum++;
+            }
+            /*if (contrasena.charAt(i)>=97 && contrasena.charAt(i)<=122){
                 contMinus+=1;
             }
             else {
@@ -62,9 +87,12 @@ public class Password { //Crear una clase Password que contenga atributos de lon
             }
             else {
                contNum+=1; 
-            }
+            }*/
         }
-    }
-        return contMayus > SECURE_PASSWD_MAYUS && contMinus > SECURE_PASSWD_MINUS && contNum > SECURE_PASSWD_NUM ? true : false;
+        if (contMayus > SECURE_PASSWD_MAYUS && contMinus > SECURE_PASSWD_MINUS && contNum > SECURE_PASSWD_NUM) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/Ejercicio2/PasswordEject.java
+++ src/Ejercicio2/PasswordEject.java
@@ -1,5 +1,27 @@
 package Ejercicio2;
 
+import javax.swing.JOptionPane;
+import Ejercicio2.Password;
+
 public class PasswordEject {
-    
+
+    public static void main(String[] args) {
+        //Introducimos el tamaño del array y la longitud del password
+        String texto=JOptionPane.showInputDialog("Introducir tamaño del array");
+        int tamanio=Integer.parseInt(texto);
+  
+        texto=JOptionPane.showInputDialog("Introducir longitud del password");
+        int longitud=Integer.parseInt(texto);
+  
+        //Creamos los arrays
+        Password listaContrasena[]=new Password[tamanio];
+        boolean fortalezaContrasena[]=new boolean[tamanio];
+  
+        //Creamos objetos, indicamos si es fuerte y mostramos la contraseña y su fortaleza.
+        for(int i=0;i<listaContrasena.length;i++){
+            listaContrasena[i]=new Password(longitud);
+            fortalezaContrasena[i]=listaContrasena[i].esFuerte();
+            System.out.println(listaContrasena[i].getContrasena()+" "+fortalezaContrasena[i]);
+        }
+    }
 }
